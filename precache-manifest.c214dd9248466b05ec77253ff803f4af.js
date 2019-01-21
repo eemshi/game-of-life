44e851d2aeb1cc9d43b6431684bcbffb30ff604c
eemshi <oreneemshi@gmail.com>
2019-01-20 17:27:49 -0700

    Updates

---
+++ precache-manifest.c214dd9248466b05ec77253ff803f4af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "daab7b3b7973132b220c",
+    "url": "/game-of-life/static/js/main.daab7b3b.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/game-of-life/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "daab7b3b7973132b220c",
+    "url": "/game-of-life/static/css/main.a08f954b.chunk.css"
+  },
+  {
+    "revision": "43d7bc49c4282c3ee64dd4d4c630adbf",
+    "url": "/game-of-life/index.html"
+  }
+];
